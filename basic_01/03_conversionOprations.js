af11ff597da7ba84428e4fdaed310aacff71f159
unknown <Mahasin Tarafdar>
2024-07-27 22:16:54 +0530

    oprations

--- basic_01/03_conversionOprations.js
+++ basic_01/03_conversionOprations.js
@@ -13,4 +13,26 @@ let booleanIsLoggedin = Boolean(isLoggedIn)
 
 let someNumber = 33
 let numberString = String(someNumber)
-console.log(typeof numberString)
+//console.log(typeof numberString)
+
+
+///////////Oprations//////////////////////
+
+let valu = 3
+let negValue = -valu
+//console.log(negValue)
+//console.log(2*2)
+
+// let str1 = "David"
+// let str2 = " Willy"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(+true)
+// let num1, num2, num3
+// num1 = num2 =num3 = 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
