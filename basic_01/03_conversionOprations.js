57ff1bf332110802180565e53bad5688cfe8e987
unknown <Mahasin Tarafdar>
2024-07-27 21:53:40 +0530

    conversons

---
+++ basic_01/03_conversionOprations.js
@@ -0,0 +1,16 @@
+let score = 33
+
+let valueNumber = Number
+
+//console.log(typeof score);
+//console.log(typeof (valueNumber));
+//console.log(score);
+
+let isLoggedIn = 1
+let booleanIsLoggedin = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedin)
+
+
+let someNumber = 33
+let numberString = String(someNumber)
+console.log(typeof numberString)
